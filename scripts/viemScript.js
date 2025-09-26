2b5b1dbdd0f0fc9dca4e7cd42bca4cdf778f4ab8
morph <artemkruglikov370@gmail.com>
2025-09-26 15:48:43 +0700

    Revert "extended my viemScript"
    This reverts commit d0d0dead33d77a41ec2a8b6b52b1c56ad06e8aee.

--- scripts/viemScript.js
+++ scripts/viemScript.js
@@ -1,83 +1,26 @@
 // scripts/read-balance.ts
-import { createPublicClient, http, formatEther, createWalletClient, parseEther } from 'viem';
-import { arbitrum, mainnet, ronin, sepolia, hardhat } from 'viem/chains';
-import { privateKeyToAccount } from 'viem/accounts';
+import { createPublicClient, http, formatEther } from 'viem';
+import { arbitrum, mainnet, ronin } from 'viem/chains';
+import { config } from 'dotenv';
+import '../abi.js';
 import abi from "../abis/abi.json"
 
-const transport = http("http://127.0.0.1:8545/")
-const accountPrivateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-const contractAddress = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9"
-
-const privateKey = privateKeyToAccount(accountPrivateKey)
-
+const client = createPublicClient({
+  chain: arbitrum,
+  transport: http("https://arb-mainnet.g.alchemy.com/v2/FibUNQuBmHPDB5RMhl6z5") 
+  
+});
 
 
 async function main() {
-    const client = createPublicClient({
-      chain: hardhat,
-      transport
-    })
 
-    const clientWallet = createWalletClient({
-      account: privateKey,
-      chain: hardhat,
-      transport
-    })
-
-    // const owner = await client.readContract({
-    //   address: contractAddress,
-    //   abi,
-    //   functionName: "owner"
-    // })
-
-    // const showPrice = await client.readContract({
-    //   address: contractAddress,
-    //   abi,
-    //   functionName: "defaultPrice"
-    // }) 
-    // await console.log(`Default price now: ${showPrice}`);
-    
-
-    // const setPrice = await clientWallet.writeContract({
-    //   address: contractAddress,
-    //   abi,
-    //   functionName: "setterPrice",
-    //   args: [parseEther("0.08")]
-    // })
-
-    // const result = await client.waitForTransactionReceipt({hash: setPrice})
-    // console.log(`current status: `, result.status);
-    // console.log(`Owner name: ${owner}, default price of buying domen number: ${formatEther(showPrice)}`);
-    const showPrice = await client.readContract({
-      address: contractAddress,
-      abi,
-      functionName: "defaultPrice"
-    });
-    
-    console.log(`Старая цена: ${formatEther(showPrice)} ETH`);
-    
-    const txHash = await clientWallet.writeContract({
-      address: contractAddress,
-      abi,
-      functionName: "setterPrice",
-      args: [parseEther("0.005")]
-    });
-    
-    console.log("Транзакция отправлена:", txHash);
-    
-    const receipt = await client.waitForTransactionReceipt({ hash: txHash });
-    console.log("Статус транзакции:", receipt.status);
-    
-    const newPrice = await client.readContract({
-      address: contractAddress,
-      abi,
-      functionName: "defaultPrice"
-    });
-    
-    console.log(`Новая цена: ${formatEther(newPrice)} ETH`); 
-
+  const balance = await client.getBalance({
+    address: '0x3003760202C0367c60391c12CF7045eD4Ec617Fd' 
+  });
+  const something = await client.getChainId()
+  console.log(`Баланс: ${formatEther(balance)} RONIN`, `chainId: ${something}`);
+  
 }
-
 
 main()
     .then(() => process.exit(0)) 
